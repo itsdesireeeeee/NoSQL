996adda23b267e1a489af582b2464791b2b1a743
Desiree <desiree.garcia@berkeley.edu>
2024-04-24 22:11:44 -0700

    Testing framework isnt working, pushing to autograder to see progress until I can get a TA to look at my laptop.

--- query/q1i.js
+++ query/q1i.js
@@ -1,5 +1,16 @@
 // Task 1i
 
-db.todo.aggregate([
+db.keywords.aggregate([
     // TODO: Write your query here
+    {$match:
+        { "keywords.name": { $in:["marvel comic", "mickey mouse"]}}
+    },
+    { $sort: { movieId: 1}
+    },
+    { $project: {"movieId": 1} //not sure if i need "keywords": 0
+    }
+
 ]);
+//  Find the IDs of all movies labeled with the keyword "mickey mouse" or "marvel comic"
+//by writing a query on the keywords collection. Order your output in ascending order of movieId
+
