2a3aa029dd986272344006efc2fded15b7e80891
Desiree <desiree.garcia@berkeley.edu>
2024-04-28 21:48:43 -0700

    All done, last commit of senior year!

--- query/q3ii.js
+++ query/q3ii.js
@@ -1,5 +1,22 @@
 // Task 3ii
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.credits.aggregate([
+    // TODO: DONE
+    {$unwind: "$crew"},
+    {$match: {"crew.id": 5655, "crew.job": "Director"}},
+    {$project: {cast: 1}},
+    {$unwind: "$cast"},
+    {$group: {_id: "$cast.id",name: {$first: "$cast.name"}  ,count: {$sum: 1}}},
+    {$sort: {count: -1}},
+    {$limit: 5},
+    {$project: {_id:0, count: 1, id: "$_id", name: 1}},
+    {$sort: {count: -1, id: 1}}
+
+
+
 ]);
+
+//Find the 5 actors who have appeared the most often in movies
+//actor's name, id, and the number of times the actor has collaborated
+//Order in descending order of the number of collaborations.
+ //Break ties in ascending order of the actor's id.
