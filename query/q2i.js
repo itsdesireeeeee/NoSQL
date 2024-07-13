9b2402d2393eea35fe4ca839edf04cb7302cabb7
Desiree Garcia <77808966+itsdesireeeeee@users.noreply.github.com>
2024-07-12 18:23:04 -0700

    Update q2i.js question description

--- query/q2i.js
+++ query/q2i.js
@@ -1,4 +1,18 @@
 // Task 2i
+// Using IMDb's Weighted Rating (WR) which assigns movies a score based on demographic filtering, return the 20 highest-rated movies according to this formula:
+//  WR = (v/(v+m))R + (m/(v+m))C
+    // where v is the number of votes for the movie, m is the minimum votes required to be listed in the chart,
+    // R is the average rating of the movie (stored in the field vote_average), and C is the mean vote across the whole report (this value is approx. 7)
+//Assume the minimum number of votes required to be listed is 1838.
+
+// The output should contain three fields:
+    // {"title": <string>, "vote_count": <number>, "score": <number>}
+// where 'score' is the WR for the associated movie rounded to two decimal places.
+
+// Sort in descending order of score, and break ties in descending order of vote_count and ascending order of title.
+
+
+
 
 db.movies_metadata.aggregate([
     // TODO: DONE
@@ -15,14 +29,3 @@ db.movies_metadata.aggregate([
     {$limit: 20}
 
 ]);
-
-//assume the minimum votes required to be listed is 1838.
-//Return the 20 highest rated movies according to this formula. The output should contain three fields:
- //title with the title of the movie, vote_count with the number of votes the movie received,
- //and score which contains the WR for the associated movie rounded to two decimal places.
- //Sort in descending order of score,
- // and break ties in descending order of vote_count and ascending order of title
-
- //{$add: ["$vote_average", 7]}
-
- //$multiply: [{$divide: [1838, {$add: ["$vote_count", 1838]}]}, 7]
