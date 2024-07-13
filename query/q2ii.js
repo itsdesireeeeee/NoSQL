a82a4cb3f99427759012e2a506404b1ed1fe1ef0
Desiree Garcia <77808966+itsdesireeeeee@users.noreply.github.com>
2024-07-12 18:27:17 -0700

    Update q2ii.js and comment clean up

--- query/q2ii.js
+++ query/q2ii.js
@@ -1,8 +1,14 @@
 // Task 2ii
+// We want to see the 20 most common words (length > 3) across all taglines in descending order.
+// We will limit the words to length >3 to remove filler words, prepositions, and some pronouns. 
+// We also want to trim off any surrounding punctuation in a word and set all words to lowercase 
+// Do not trim off any other punctuation character that is not mentioned. Order your output in descending order of count.
+//Your output documents should have the following fields:
+    //{ "_id": <string>, "count": <number>}
 
 db.movies_metadata.aggregate([
     // TODO: DONE
-    //make sure string exists found from stack overflow
+    //make sure string exists
     {$project: {"_id": 0,
         word: {$split: [{$toLower: {$trim: {input: "$tagline", chars: ".,?! " }}}, " " ]}
     }},
@@ -14,9 +20,4 @@ db.movies_metadata.aggregate([
     {$sort: {count: -1}},
     {$limit: 20}
 
-
 ]);
-
-//{$split: [{$toLower: {$trim: {input: "$tagLine", chars: ".,?!" }}}, " " ]}
-
-
